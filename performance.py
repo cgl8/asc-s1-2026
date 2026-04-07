7210f9e86a7f15ece2e17c4e6a66436558e9680c
cgl8 <122022040+cgl8@users.noreply.github.com>
2026-04-07 17:10:04 +0000

    perf

---
+++ performance.py
@@ -0,0 +1,23 @@
+import subprocess
+import sys
+filename = "./ts/performance.hybrid"
+for clause_length in range(2, 100):
+    file = open(filename, 'w')
+    file.write("p hybrid " + str(clause_length) + " 1\n")
+    for var in range(1, clause_length+1):
+        file.write(str(var) + " ")
+    file.write("0")
+    file.close()
+
+    try:
+        afsat = subprocess.run(["python3", "afsat.py", filename, "-d", "INFO"], 
+        capture_output=True, 
+        text=True,
+        check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error running transform.py: {e}", file=sys.stderr)
+        print(f"stderr: {e.stderr}", file=sys.stderr)
+        exit(1)
+    #print(afsat.stderr)
+    time = afsat.stderr.strip().split('\n')[-1]
+    print(time)
